c291eb75f6ea92559331f1305b1edc027f00c017
Federico Milone <fedeemilo91@gmail.com>
2020-02-07 23:11:54 -0300

    creating posts routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,43 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { apiResponse: '' };
+		this.state = { apiResponse: '', apiPosts: '' };
 	}
 
 	callAPI() {
-		fetch('http://localhost:9000/testAPI')
-			.then((res) => res.text())
-			.then((res) => this.setState({ apiResponse: res }))
-			.catch(err => err);
+		axios
+			.get('http://localhost:9000/testAPI')
+			.then((res) => {
+				const test = res.data;
+				this.setState({ apiResponse: test });
+			})
+			.catch((err) => err);
+	}
+
+	callPosts() {
+		axios
+			.get('http://localhost:9000/posts')
+			.then((res) => {
+				const posts = res.data;
+				this.setState({ apiPosts: posts });
+			})
+			.catch((err) => err);
 	}
 
 	componentDidMount() {
 		this.callAPI();
+		this.callPosts();
 	}
 
 	render() {
 		return (
 			<div className='App'>
-				<header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
 				<h1>{this.state.apiResponse}</h1>
+				<h2>{this.state.apiPosts}</h2>
 			</div>
 		);
 	}
